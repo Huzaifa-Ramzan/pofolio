17d4140ac3d8ad970ebf53690615d4b6c79e2bb8
Huzaifa Ramzan <84973513+Huzaifa-Ramzan@users.noreply.github.com>
2021-09-18 16:52:53 +0500

    Update savedata.php

--- savedata.php
+++ savedata.php
@@ -5,12 +5,7 @@
 	$con=mysqli_connect('localhost','root','','myportfolio') or die('not connect');			
 	$sql="insert into contact (fname,email) values ('{$fname}','{$email}')";
     mysqli_query($con,$sql);
-	
-	$to="huzaifa.1ramzan@gmail.com";
-	$subject='project';
-	$header="From:$email";
-	mail($to,$subject,$email,$header);
-		
+
 	header("Location:http://localhost/myportfolio/");
     
 						
