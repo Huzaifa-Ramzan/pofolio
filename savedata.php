d630166fa295f6b34215fefb5f11820283673470
Huzaifa Ramzan <84973513+Huzaifa-Ramzan@users.noreply.github.com>
2022-02-25 19:06:29 +0500

    Update savedata.php

--- savedata.php
+++ savedata.php
@@ -1,10 +1,12 @@
 <?php
-	
+	$from="huzaifa@netlify.app";
+	$to="huzaifa.1ramzan@gmail.com";
 	$fname=$_POST['fname'];
 	$email=$_POST['email']; 
 	$con=mysqli_connect('localhost','root','','myportfolio') or die('not connect');			
 	$sql="insert into contact (fname,email) values ('{$fname}','{$email}')";
-    mysqli_query($con,$sql);
+  	mysqli_query($con,$sql);
+	mail($to,$from,$suject,$email);
 
 	header("Location:http://localhost/myportfolio/");
     
